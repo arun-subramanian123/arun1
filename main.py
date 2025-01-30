f80b6a5c4e80d7aa07019b2b73f7aa072d6159fc
arun-subramanian123 <arun.subramanian@aig.com>
2025-01-30 19:29:26 +0000

    end of class

--- main.py
+++ main.py
@@ -4,5 +4,18 @@ w = "world"
 # print(h,w)
 # print(h+w)
 
-n1 = 5
-print(h+str(n1))
+# n1 = 5
+# print(h+str(n1))
+
+n1=5
+n2=5
+
+if n1<n2:
+    print("Lesser")
+    print("<"*6)
+elif n1==n2:
+    print('Equal')
+    print('====')
+else:
+    print("Greater")
+    print(">"*len("Greater"))
